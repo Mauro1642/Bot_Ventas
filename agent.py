ec1e0283d3f62c845c6367371fb77790c9a6c94e
Mauro Chavez <mauchacf@gmail.com>
2026-06-11 18:22:08 -0300

    fixed

--- agent.py
+++ agent.py
@@ -2,7 +2,6 @@ import os
 from dotenv import load_dotenv
 from langchain_classic.agents import AgentExecutor
 from langchain_classic.agents import create_tool_calling_agent
-from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_groq import ChatGroq
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.messages import HumanMessage, AIMessage
